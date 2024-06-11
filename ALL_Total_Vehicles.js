3b4f70f64accded15b724d49cc8eb9ebed76e101
JR-2004 <143631767+JR-2004@users.noreply.github.com>
2024-06-11 20:10:37 +0200

    Update ALL_Total_Vehicles.js

--- ALL_Total_Vehicles.js
+++ ALL_Total_Vehicles.js
@@ -1,15 +1,34 @@
 // ==UserScript==
 // @name         {All}Total Vehicles
 // @namespace    https://www.leitstellenspiel.de/
-// @version      0.1
+// @version      1.0
 // @description:de  Zählt Fahrzeuge an der Einsatzstelle
 // @description     Creates a counter for all vehicles on scene
 // @author       LennardTFD & JR04
-// @match        https://www.leitstellenspiel.de/missions/*
-// @match        https://www.missionchief.com/missions/*
-// @match        https://www.meldkamerspel.com/missions/*
-// @updateURL    https://github.com/LennardTFD/LeitstellenspielScripte/raw/master/LSS_VehicleCounter/vehicleCounter.user.js
-// @downloadURL  https://github.com/LennardTFD/LeitstellenspielScripte/raw/master/LSS_VehicleCounter/vehicleCounter.user.js
+// @match        https://www.leitstellenspiel.de/profile/*
+// @match		 https://www.meldkamerspel.com/profile/*
+// @match		 https://www.missionchief.com/profile/*
+// @match		 https://www.missionchief.co.uk/profile/*
+// @match		 https://www.missionchief-australia.com/profile/*
+// @match 		 https://www.centro-de-mando.es/profile/*
+// @match 	     https://www.operatorratunkowy.pl/profile/*
+// @match		 https://www.larmcentralen-spelet.se/profile/*
+// @match		 https://www.operatore112.it/profile/*
+// @match		 https://www.operateur112.fr/profile/*
+// @match		 https://www.dispetcher112.ru/profile/*
+// @match		 https://www.alarmcentral-spil.dk/profile/*
+// @match		 https://www.nodsentralspillet.com/profile/*
+// @match		 https://www.operacni-stredisko.cz/profile/*
+// @match		 https://www.jogo-operador112.com/profile/*
+// @match		 https://www.operador193.com/profile/*
+// @match		 https://www.dyspetcher101-game.com/profile/*
+// @match		 https://www.missionchief-japan.com/profile/*
+// @match		 https://www.missionchief-korea.com/profile/*
+// @match		 https://www.jocdispecerat112.com/profile/*
+// @match		 https://www.hatakeskuspeli.com/profile/*
+// @match		 https://www.dispecerske-centrum.com/profile/*
+// @downloadURL  https://raw.githubusercontent.com/JR-2004/LSSScripts/main/ALL_Total_Vehicles.js
+// @updateURL    https://raw.githubusercontent.com/JR-2004/LSSScripts/main/ALL_Total_Vehicles.js
 // @grant        GM_addStyle
 // ==/UserScript==
 
@@ -56,7 +75,7 @@ GM_addStyle(`
 (function() {
     'use strict';
     //Create Label with Vehicles
-    var vehOnSite = $("<span class='amount_of_people_label tooltipVehAmount'>Fahrzeuge <span class='label' style='background-color: grey' id='vehAmount'>0<</span><span class='tooltiptextVehAmount'>Vor Ort: <span id='vehAmountSite'>0</span><br>Anfahrt: <span id='vehAmountEnroute'>0</span></span></span>");
+    var vehOnSite = $("<span class='amount_of_people_label tooltipVehAmount'>Vehicles <span class='label' style='background-color: grey' id='vehAmount'>0<</span><span class='tooltiptextVehAmount'>Vor Ort: <span id='vehAmountSite'>0</span><br>Anfahrt: <span id='vehAmountEnroute'>0</span></span></span>");
     $("#amount_of_people").append(vehOnSite);
 
     //Count Vehicles on Site
