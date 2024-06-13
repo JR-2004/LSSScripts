816b10f111f49847d1c17c1d72870f76aac764ad
JR-2004 <143631767+JR-2004@users.noreply.github.com>
2024-06-13 15:05:55 +0200

    Update ALL_Total_Vehicles.js

--- ALL_Total_Vehicles.js
+++ ALL_Total_Vehicles.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         {All}Total Vehicles
 // @namespace    https://www.leitstellenspiel.de/
-// @version      1.1.2
+// @version      1.1.3
 // @description:de  Zählt Fahrzeuge an der Einsatzstelle
 // @description     Creates a counter for all vehicles on scene
 // @author       LennardTFD & JR04
@@ -29,9 +29,6 @@
 // @match		 https://www.dispecerske-centrum.com/missions/*
 // @downloadURL  https://raw.githubusercontent.com/JR-2004/LSSScripts/main/ALL_Total_Vehicles.js
 // @updateURL    https://raw.githubusercontent.com/JR-2004/LSSScripts/main/ALL_Total_Vehicles.js
-// @icon        https://raw.githubusercontent.com/JR-2004/LSSScripts/main/icon.jpg
-// @run-at      document-idle
-// @grant       none
 // ==/UserScript==
 
 GM_addStyle(`
