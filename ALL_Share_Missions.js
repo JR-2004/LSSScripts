e1d21f491bae0b1d39fea3d42c0d70690dc7eca5
JR-2004 <143631767+JR-2004@users.noreply.github.com>
2024-06-21 20:42:30 +0200

    Update ALL_Share_Missions.js

--- ALL_Share_Missions.js
+++ ALL_Share_Missions.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        {All}Share Missions
 // @namespace   bos-ernie.leitstellenspiel.de
-// @version     1.1
+// @version     1.1.1
 // @license     BSD-3-Clause
 // @author      BOS-Ernie & JR04
 // @description:de Fügt einen Button zum schnellen Teilen der eigenen Einsätze aus der Einsatzliste hinzu.
@@ -31,7 +31,6 @@
 // @icon        https://raw.githubusercontent.com/JR-2004/LSSScripts/main/icon.png
 // @run-at      document-idle
 // @grant       none
-// @icon        https://raw.githubusercontent.com/JR-2004/LSSScripts/main/logo.png
 // @downloadURL  https://raw.githubusercontent.com/JR-2004/LSSScripts/main/ALL_Share_Missions.js
 // @updateURL    https://raw.githubusercontent.com/JR-2004/LSSScripts/main/ALL_Share_Missions.js
 // ==/UserScript==
