013bd34b7810199c8c0278d322fdc7e62e8a1d79
JR-2004 <143631767+JR-2004@users.noreply.github.com>
2024-07-01 13:18:55 +0200

    Update ALL_Schoolingsfilter.js

--- ALL_Schoolingsfilter.js
+++ ALL_Schoolingsfilter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         {ALL}Searchbar Schoolings page
 // @namespace    https://www.meldkamerspel.com/
-// @version      1.1.2
+// @version      1.1.3
 // @description  Adds a searchbar to all Missionchief versions for filtering buildings
 // @author       JR04
 // @match           https://www.leitstellenspiel.de/schoolings/*
@@ -26,6 +26,28 @@
 // @match                        https://www.jocdispecerat112.com/schoolings/*
 // @match                        https://www.hatakeskuspeli.com/schoolings/*
 // @match                        https://www.dispecerske-centrum.com/schoolings/*
+// @match           https://www.leitstellenspiel.de/buildings/*
+// @match                        https://www.meldkamerspel.com/buildings/*
+// @match           https://www.missionchief.com/buildings/*
+// @match                        https://www.missionchief.co.uk/buildings/*
+// @match                        https://www.missionchief-australia.com/buildings/*
+// @match                       https://www.centro-de-mando.es/buildings/*
+// @match                 https://www.operatorratunkowy.pl/buildings/*
+// @match                        https://www.larmcentralen-spelet.se/buildings/*
+// @match                        https://www.operatore112.it/buildings/*
+// @match                        https://www.operateur112.fr/buildings/*
+// @match                        https://www.dispetcher112.ru/buildings/*
+// @match                        https://www.alarmcentral-spil.dk/buildings/*
+// @match                        https://www.nodsentralspillet.com/buildings/*
+// @match                        https://www.operacni-stredisko.cz/buildings/*
+// @match                        https://www.jogo-operador112.com/buildings/*
+// @match                        https://www.operador193.com/buildings/*
+// @match                        https://www.dyspetcher101-game.com/buildings/*
+// @match                        https://www.missionchief-japan.com/buildings/*
+// @match                        https://www.missionchief-korea.com/buildings/*
+// @match                        https://www.jocdispecerat112.com/buildings/*
+// @match                        https://www.hatakeskuspeli.com/buildings/*
+// @match                        https://www.dispecerske-centrum.com/buildings/*
 // @icon   https://raw.githubusercontent.com/JR-2004/LSSScripts/main/icon.png
 // @downloadURL     https://raw.githubusercontent.com/JR-2004/LSSScripts/main/ALL_Schoolingsfilter.js
 // @updateURL       https://raw.githubusercontent.com/JR-2004/LSSScripts/main/ALL_Schoolingsfilter.js
